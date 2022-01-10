2a78e1cd3e309246feae50d0f84c581e06bfe475
Rin <62606765+chuangyu-hscy@users.noreply.github.com>
2022-01-10 19:00:34 +1100

    Added input type for input box

--- components/login/LoginInfo.tsx
+++ components/login/LoginInfo.tsx
@@ -7,6 +7,8 @@ import {
   Input,
   Stack,
   InputLeftElement,
+  InputGroup,
+  InputRightElement,
 } from "@chakra-ui/react";
 import { grey } from "@mui/material/colors";
 import { MdBuild, MdCall, MdConfirmationNumber } from "react-icons/md";
@@ -14,22 +16,14 @@ import { MdBuild, MdCall, MdConfirmationNumber } from "react-icons/md";
 import { FcGoogle } from "react-icons/fc";
 
 import { ChakraProvider } from "@chakra-ui/react";
+import React, { useState } from "react";
 
 // import required stylesheet
 import styles from "../../styles/Login.module.scss";
 
 // define the login info function
 export default function LoginInfo() {
-  const property = {
-    imageUrl: "https://bit.ly/2Z4KKcF",
-    imageAlt: "Rear view of modern home with pool",
-    beds: 3,
-    baths: 2,
-    title: "Modern home in city center in the heart of historic Los Angeles",
-    formattedPrice: "$1,900.00",
-    reviewCount: 34,
-    rating: 4,
-  };
+  const [show, setShow] = useState(false);
 
   return (
     <ChakraProvider>
@@ -58,22 +52,25 @@ export default function LoginInfo() {
         <h3 className={styles.loginH3}>
           <b>Email*:</b>
         </h3>
-        <Input
-          placeholder="Please Enter your Email"
-          size="lg"
-          variant="outline"
-        />
+        <Input pr="4.5rem" type={"email"} placeholder="Enter email" />
         <br />
         <br />
         <h3 className={styles.loginH3}>
           <b>Password*:</b>
         </h3>
 
-        <Input
-          placeholder="Please Enter your Password"
-          size="lg"
-          variant="outline"
-        />
+        <InputGroup size="md">
+          <Input
+            pr="4.5rem"
+            type={show ? "text" : "password"}
+            placeholder="Enter password"
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={() => setShow(!show)}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
 
         <br />
         {/* <hr /> */}
