7b95bfbdc9269eb0bc7a9e5e0056dd3fe8d00570
Rin <62606765+chuangyu-hscy@users.noreply.github.com>
2022-01-20 22:55:45 +1100

    Update LoginIllustration.tsx

--- components/login/LoginIllustration.tsx
+++ components/login/LoginIllustration.tsx
@@ -1,12 +1,16 @@
 // import required stylesheet
-import styles from "../../styles/Login.module.scss";
+import styles from "../../styles/login/Login.module.scss";
 
 export default function LoginIllustration(): JSX.Element {
   return (
     <div className={styles.illustration}>
       <div className={styles.illustrationTextContainer}>
-        <h1 className={styles.h1}>随便の前端技术堆积站</h1>
-        <p className={styles.p}>@rNLKJA, kwitter777</p>
+        <h1 className={styles.h1} hidden>
+          随便の前端技术堆积站
+        </h1>
+        <p className={styles.p} hidden>
+          @rNLKJA, kwitter777
+        </p>
       </div>
     </div>
   );
